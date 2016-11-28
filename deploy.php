837d5ab5b667a330053c4f228f01da428659743f
Matteo Rosati <mrosati@h-art.com>
2016-11-28 20:35:11 +0100

    initial commit

---
+++ deploy.php
@@ -0,0 +1,30 @@
+<?php
+namespace Deployer;
+require 'recipe/drupal8.php';
+
+// Configuration
+
+set('repository', 'git@domain.com:username/repository.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+
+add('writable_dirs', []);
+
+// Servers
+
+server('production', 'domain.com')
+    ->user('username')
+    ->identityFile()
+    ->set('deploy_path', '/var/www/domain.com');
+
+
+// Tasks
+
+desc('Restart PHP-FPM service');
+task('php-fpm:restart', function () {
+    // The user must have rights for restart service
+    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+    run('sudo systemctl restart php-fpm.service');
+});
+after('deploy:symlink', 'php-fpm:restart');
